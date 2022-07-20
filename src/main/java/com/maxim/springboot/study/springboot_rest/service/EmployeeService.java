dd06fe49e0b888429f1672aba900b2b510fd05f8
Максим <zaruv14@yandex.ru>
2022-07-21 01:14:55 +0300

    Spring boot restAPI service

---
+++ src/main/java/com/maxim/springboot/study/springboot_rest/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.maxim.springboot.study.springboot_rest.service;
+
+
+import com.maxim.springboot.study.springboot_rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
---
+++ src/test/java/com/maxim/springboot/study/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxim.springboot.study.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
